c8e695e122131a67eeaea48c84fb756e22b0c375
Andrey Kashapov <kashapovandrey@gmail.com>
2018-11-26 21:08:18 +0300

    Basics Links

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React from "react";
+import Link from "next/link";
+import Router from "next/router";
 
 const authIndexPage = () => (
   <div>
     <h1>The Auth Page</h1>
+    <p>
+      Go to <Link href="/">Main</Link>
+    </p>
+    <button onClick={() => Router.push("/")}>Go to Main</button>
   </div>
 );
 
